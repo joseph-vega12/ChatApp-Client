45bac4641e85bbc3ae4b4e91cc862895b74f344a
Joseph Vega <josephvega@Josephs-Mini.lan>
2022-01-16 23:16:35 -0800

    added user login functionality

--- src/Authentication/Login.js
+++ src/Authentication/Login.js
@@ -1,7 +1,52 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 function Login() {
+  const [input, setInput] = useState({ username: "", password: "" });
+  let navigate = useNavigate();
+
+  const onChange = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:4000/auth/login", {
+        username: e.target.username.value,
+        password: e.target.password.value,
+      })
+      .then((response) => {
+        localStorage.setItem("token", response.data.token);
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <>
-      <h1>Please Login</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Enter Username"
+          label="username"
+          onChange={onChange}
+          value={input.username}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter Password"
+          label="password"
+          onChange={onChange}
+          value={input.password}
+        />
+        <button>Login</button>
+      </form>
     </>
   );
 }
