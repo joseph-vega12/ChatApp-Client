e2e2cf51e42063c5da3aca797a47353c17803c34
Joseph Vega <josephvega@Josephs-Mini.lan>
2022-01-26 14:06:53 -0800

    added bootstrap styling to Chat.js and NavigationBar.js, and relocated logout

---
+++ src/Common/NavigationBar.js
@@ -0,0 +1,34 @@
+import { useNavigate } from "react-router-dom";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import * as Icon from "react-bootstrap-icons";
+function NavigationBar() {
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    navigate("/");
+  };
+
+  return (
+    <Navbar className="p-4" collapseOnSelect bg="light" variant="light">
+      <Navbar.Brand href="#">ChatApp</Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse className="justify-content-end">
+        <Nav>
+          <Nav.Link href="#">
+            <Icon.PersonCircle size={27} />
+          </Nav.Link>
+          <NavDropdown title={<Icon.ThreeDotsVertical size={27} />} align="end">
+            <NavDropdown.Item>Profile</NavDropdown.Item>
+            <NavDropdown.Item>Settings</NavDropdown.Item>
+            <NavDropdown.Item>FAQS</NavDropdown.Item>
+            <NavDropdown.Item>Terms Of Use</NavDropdown.Item>
+            <NavDropdown.Item onClick={logout}>Logout</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default NavigationBar;
--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,13 +1,22 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import Nav from "../Common/NavigationBar";
 import Messages from "../Components/Messages";
 import io from "socket.io-client";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  InputGroup,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+import * as Icon from "react-bootstrap-icons";
 
 function Chat() {
   const [rooms, setRooms] = useState([]);
-  const [selectedRoom, setSelectedRoom] = useState(null);
-  const navigate = useNavigate();
+  const [selectedRoom, setSelectedRoom] = useState(1);
 
   useEffect(() => {
     axios
@@ -24,33 +33,53 @@ function Chat() {
       });
   }, []);
 
-  const logout = () => {
-    localStorage.removeItem("token");
-    navigate("/");
-  };
-
   const selectRoom = (roomId) => {
     setSelectedRoom(roomId);
   };
 
   return (
-    <div className="Container">
-      <h4 onClick={logout}>Logout</h4>
-      <div className="Rooms">
-        <h1>Rooms</h1>
-        {rooms.map((room) => (
-          <h3
-            key={room.id}
-            onClick={() => {
-              selectRoom(room.id);
-            }}
-          >
-            {room.roomname}
-          </h3>
-        ))}
-        <Messages selectedRoom={selectedRoom} />
-      </div>
-    </div>
+    <>
+      <Container fluid>
+        <Row className="h-100">
+          <Col sm={6} md={5} lg={4} xl={3} xxl={3}>
+            <Col className="d-flex justify-content-between m-4" lg="auto">
+              <h3 className="m-0">Rooms</h3>
+              <h6 className="m-0 p-2">+ Create new room</h6>
+            </Col>
+            <InputGroup className="pb-4 ms-auto me-auto w-100">
+              <Button variant="outline-secondary" id="button-addon1">
+                <Icon.Search />
+              </Button>
+              <FormControl />
+            </InputGroup>
+            {rooms.map((room) => (
+              <Card
+                className="ms-auto me-auto mb-2 w-80"
+                bg="light"
+                key={room.id}
+                onClick={() => {
+                  selectRoom(room.id);
+                }}
+              >
+                <Card.Body className="d-flex p-4 justify-content-start">
+                  <div className="UserPicture">
+                    <Icon.PersonCircle className="me-3" size={40} />
+                  </div>
+                  <div className="Rooms">
+                    <Card.Title>{room.roomname}</Card.Title>
+                    <Card.Subtitle>Hello This is a test Room</Card.Subtitle>
+                  </div>
+                </Card.Body>
+              </Card>
+            ))}
+          </Col>
+          <Col lg={9} className="p-0 border-start">
+            <Nav />
+            <Messages selectedRoom={selectedRoom} />
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
